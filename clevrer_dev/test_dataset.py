5494990a1f33818610366893b04157d84f0fab74
gabrielsluz <gabrielsluz1999@gmail.com>
2021-02-05 13:59:07 -0300

    Dataset working

--- clevrer_dev/test_dataset.py
+++ clevrer_dev/test_dataset.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from slowfast.datasets.clevrer import ClevrerFrame
+import matplotlib.pyplot as plt
 import torch
 
 from slowfast.utils.parser import load_config, parse_args
@@ -14,4 +15,8 @@ logging.setup_logging(cfg.OUTPUT_DIR)
 frame_dataset = ClevrerFrame(cfg, 'train')
 print(len(frame_dataset))
 for i in range(len(frame_dataset)):
-    print(frame_dataset[i])
+    print(frame_dataset[i].size())
+
+tensor_image = frame_dataset[0].permute(1,2,0)
+plt.imshow(tensor_image)
+plt.savefig('saved_figure.png')
