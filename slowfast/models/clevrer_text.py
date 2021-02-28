446d0dde69efa5e1c72e104798c4fd900a519a6d
gabrielsluz <gabrielsluz1999@gmail.com>
2021-02-28 16:47:55 -0300

    Dropout in lstm needs multi layer

--- slowfast/models/clevrer_text.py
+++ slowfast/models/clevrer_text.py
@@ -78,7 +78,7 @@ class TEXT_LSTM(nn.Module):
         self.num_directions = 1
         self.LSTM = torch.nn.LSTM(
             input_size=self.enc_dim, hidden_size=self.hid_st_dim, num_layers=self.num_layers,
-            bias=True, batch_first=True, dropout=0.2, bidirectional=(self.num_directions == 2)
+            bias=True, batch_first=True, dropout=0, bidirectional=(self.num_directions == 2)
         )
         #Prediction head MLP
         hid_dim = 2048
