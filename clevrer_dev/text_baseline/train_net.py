e4afa12897b8fa8d7243ad5b7a5df5c92a0b8759
gabrielsluz <gabrielsluz1999@gmail.com>
2021-03-04 01:39:34 -0300

    Testing for double dataset

--- clevrer_dev/text_baseline/train_net.py
+++ clevrer_dev/text_baseline/train_net.py
@@ -6,6 +6,7 @@ import numpy as np
 import pprint
 import torch
 import copy
+from torch.utils.data import DataLoader
 
 import slowfast.models.losses as losses
 import slowfast.models.optimizer as optim
@@ -13,10 +14,9 @@ import slowfast.utils.checkpoint as cu
 import slowfast.utils.logging as logging
 import slowfast.utils.metrics as metrics
 import slowfast.utils.misc as misc
-from slowfast.datasets import loader
 from slowfast.utils.meters import ClevrerTrainMeter, ClevrerValMeter
 #Clevrer specific
-from slowfast.datasets.clevrer_text import Clevrertext
+from slowfast.datasets.clevrer_text import Clevrertext, Clevrertext_join, Clevrertext_des, Clevrertext_mc
 from slowfast.models.build import MODEL_REGISTRY
 
 logger = logging.get_logger(__name__)
@@ -44,21 +44,36 @@ def train_epoch(
     data_size = len(train_loader)
     
     for cur_iter, sampled_batch in enumerate(train_loader): 
-        is_des = sampled_batch['question_dict']['is_des']
-        question = sampled_batch['question_dict']['question']
-        ans = sampled_batch['question_dict']['ans']
+        #Samples 2 batches. One for des and one for mc
+        #There are much more des, then some batches are only des
+        des_batch = sampled_batch['des']
+        des_q = des_batch['question_dict']['question']
+        des_ans = des_batch['question_dict']['ans']
+        des_len = des_batch['question_dict']['len']
         # Transfer the data to the current GPU device.
         if cfg.NUM_GPUS:
-            is_des = is_des.cuda(non_blocking=True)
-            question = question.cuda(non_blocking=True)
-            ans = ans.cuda()
+            des_q = des_q.cuda(non_blocking=True)
+            des_ans = des_ans.cuda()
+            des_len = des_len.cuda(non_blocking=True)
+        
+        has_mc = sampled_batch['has_mc']
+        if has_mc:
+            mc_batch = sampled_batch['mc']
+            mc_q = mc_batch['question_dict']['question']
+            mc_ans = mc_batch['question_dict']['ans']
+            mc_len = mc_batch['question_dict']['len']
+            if cfg.NUM_GPUS:
+                mc_q = mc_q.cuda(non_blocking=True)
+                mc_ans = mc_ans.cuda()
+                mc_len = mc_len.cuda(non_blocking=True)
 
         # Update the learning rate.
         lr = optim.get_epoch_lr(cur_epoch + float(cur_iter) / data_size, cfg)
         optim.set_lr(optimizer, lr)
 
         train_meter.data_toc()
-        #Try separating the batches
+
+        #Separated batches
         #Des
         pred_des_ans = model(des_q, True)
         des_loss_fun = losses.get_loss_func('cross_entropy')(reduction="mean")
@@ -73,31 +88,32 @@ def train_epoch(
         loss_des_val = loss
 
         #MC
-        pred_mc_ans = model(mc_q, False)
-        mc_loss_fun = losses.get_loss_func('bce_logit')(reduction="mean")
-        loss = mc_loss_fun(pred_mc_ans, mc_ans) #Multiply by 4
-        # check Nan Loss.
-        misc.check_nan_losses(loss)
-        #Backward pass
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        #Save for stats
-        loss_mc_val = loss
+        loss_mc_val = None
+        if has_mc:
+            pred_mc_ans = model(mc_q, False)
+            mc_loss_fun = losses.get_loss_func('bce_logit')(reduction="mean")
+            loss = mc_loss_fun(pred_mc_ans, mc_ans) #Multiply by 4
+            # check Nan Loss.
+            misc.check_nan_losses(loss)
+            #Backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            #Save for stats
+            loss_mc_val = loss
 
-        loss = loss_mc_val + loss_des_val
-
-        # #Non separated:
+        # #Non separated Not updated for same batch 2 questions:
         # pred_des_ans = model(des_q, True)
         # pred_mc_ans = model(mc_q, False)
         # # Explicitly declare reduction to mean.
         # des_loss_fun = losses.get_loss_func('cross_entropy')(reduction="mean")
         # mc_loss_fun = losses.get_loss_func('bce_logit')(reduction="mean")
         # # Compute the loss.
-        # loss = des_loss_fun(pred_des_ans, des_ans) + mc_loss_fun(pred_mc_ans, mc_ans)
+        # loss_des_val = des_loss_fun(pred_des_ans, des_ans)
+        # loss_mc_val = mc_loss_fun(pred_mc_ans, mc_ans)
+        # loss = loss_mc_val + loss_des_val
         # # check Nan Loss.
         # misc.check_nan_losses(loss)
-
         # # Perform the backward pass.
         # optimizer.zero_grad()
         # loss.backward()
@@ -110,30 +126,40 @@ def train_epoch(
         top1_err, top5_err = [
             (1.0 - x / pred_des_ans.size(0)) * 100.0 for x in num_topks_correct
         ]
-        diff_mc_ans = torch.abs(mc_ans - (torch.sigmoid(pred_mc_ans) >= 0.5).float()) #Errors
-        mc_opt_err = 100 * torch.true_divide(diff_mc_ans.sum(), (4*mc_q.size()[0]))
-        mc_q_err = 100 * torch.true_divide((diff_mc_ans.sum(dim=1, keepdim=True) != 0).float().sum(), mc_q.size()[0])
-        # Copy the stats from GPU to CPU (sync point).
-        loss, top1_err, top5_err, mc_opt_err, mc_q_err  = (
-            loss.item(),
-            top1_err.item(),
-            top5_err.item(),
-            mc_opt_err.item(),
-            mc_q_err.item()
-        )
-
+        if has_mc:
+            diff_mc_ans = torch.abs(mc_ans - (torch.sigmoid(pred_mc_ans) >= 0.5).float()) #Errors
+            mc_opt_err = 100 * torch.true_divide(diff_mc_ans.sum(), (4*mc_q.size()[0]))
+            mc_q_err = 100 * torch.true_divide((diff_mc_ans.sum(dim=1, keepdim=True) != 0).float().sum(), mc_q.size()[0])
+            # Copy the stats from GPU to CPU (sync point).
+            loss_des_val, loss_mc_val, top1_err, top5_err, mc_opt_err, mc_q_err  = (
+                loss_des_val.item(),
+                loss_mc_val.item(),
+                top1_err.item(),
+                top5_err.item(),
+                mc_opt_err.item(),
+                mc_q_err.item()
+            )
+            mb_size_mc = mc_q.size()[0]
+        else:
+            mc_opt_err, mc_q_err = None, None
+            mb_size_mc = None
+            loss_des_val, top1_err, top5_err = (
+                loss_des_val.item(),
+                top1_err.item(),
+                top5_err.item()
+            )
+        #top1_err, top5_err, mc_opt_err, mc_q_err, loss_des, loss_mc, lr, mb_size
         # Update and log stats.
         train_meter.update_stats(
             top1_err,
             top5_err,
             mc_opt_err,
             mc_q_err,
-            loss,
+            loss_des_val,
+            loss_mc_val,
             lr,
-            des_q.size()[0]
-            * max(
-                cfg.NUM_GPUS, 1
-            ),  # If running  on CPU (cfg.NUM_GPUS == 1), use 1 to represent 1 CPU.
+            des_q.size()[0],
+            mb_size_mc
         )
         train_meter.iter_toc()  # measure allreduce for this meter
         train_meter.log_iter_stats(cur_epoch, cur_iter)
@@ -154,14 +180,14 @@ def train_epoch(
             print("Top1_err and Top5err")
             print(top1_err, top5_err)
             print("Loss_des_val = {}".format(loss_des_val))
-
-            print(" --- Multiple Choice questions results --- ")
-            print("Mc_q")
-            print(mc_q)
-            print("Mc errors pred x ans")
-            print(torch.abs(mc_ans - (torch.sigmoid(pred_mc_ans) >= 0.5).float()))
-            print("mc_opt_err = {} \nmc_q_err = {}".format(mc_opt_err, mc_q_err))
-            print("Loss_mc_val = {}".format(loss_mc_val))
+            if has_mc:
+                print(" --- Multiple Choice questions results --- ")
+                print("Mc_q")
+                print(mc_q)
+                print("Mc errors pred x ans")
+                print(torch.abs(mc_ans - (torch.sigmoid(pred_mc_ans) >= 0.5).float()))
+                print("mc_opt_err = {} \nmc_q_err = {}".format(mc_opt_err, mc_q_err))
+                print("Loss_mc_val = {}".format(loss_mc_val))
             test_counter += 1
             if test_counter == 4: 
                 break
@@ -189,60 +215,84 @@ def eval_epoch(val_loader, model, val_meter, cur_epoch, cfg, test_imp=False):
     val_meter.iter_tic()
 
     for cur_iter, sampled_batch in enumerate(val_loader):
-        des_q = sampled_batch['question_dict']['des_q']
-        des_ans = sampled_batch['question_dict']['des_ans']
-        mc_q = sampled_batch['question_dict']['mc_q']
-        mc_ans = sampled_batch['question_dict']['mc_ans']
+        #Samples 2 batches. One for des and one for mc
+        #There are much more des, then some batches are only des
+        des_batch = sampled_batch['des']
+        des_q = des_batch['question_dict']['question']
+        des_ans = des_batch['question_dict']['ans']
+        des_len = des_batch['question_dict']['len']
         # Transfer the data to the current GPU device.
         if cfg.NUM_GPUS:
             des_q = des_q.cuda(non_blocking=True)
             des_ans = des_ans.cuda()
-            mc_q = mc_q.cuda(non_blocking=True)
-            mc_ans = mc_ans.cuda()
+            des_len = des_len.cuda(non_blocking=True)
+        
+        has_mc = sampled_batch['has_mc']
+        if has_mc:
+            mc_batch = sampled_batch['mc']
+            mc_q = mc_batch['question_dict']['question']
+            mc_ans = mc_batch['question_dict']['ans']
+            mc_len = mc_batch['question_dict']['len']
+            if cfg.NUM_GPUS:
+                mc_q = mc_q.cuda(non_blocking=True)
+                mc_ans = mc_ans.cuda()
+                mc_len = mc_len.cuda(non_blocking=True)
 
         val_meter.data_toc()
 
-        pred_des_ans = model(des_q, True)
-        pred_mc_ans = model(mc_q, False)
         # Explicitly declare reduction to mean.
         des_loss_fun = losses.get_loss_func('cross_entropy')(reduction="mean")
         mc_loss_fun = losses.get_loss_func('bce_logit')(reduction="mean")
-        # Compute the loss.
+
+        pred_des_ans = model(des_q, True)
         loss_des_val = des_loss_fun(pred_des_ans, des_ans)
-        loss_mc_val = mc_loss_fun(pred_mc_ans, mc_ans) 
-        loss = loss_des_val + loss_mc_val
+
+        loss_mc_val = None
+        if has_mc:
+            pred_mc_ans = model(mc_q, False)
+            loss_mc_val = mc_loss_fun(pred_mc_ans, mc_ans) 
+
         # Compute the errors.
         num_topks_correct = metrics.topks_correct(pred_des_ans, des_ans, (1, 5))
         # Combine the errors across the GPUs.
         top1_err, top5_err = [
             (1.0 - x / pred_des_ans.size(0)) * 100.0 for x in num_topks_correct
         ]
-        diff_mc_ans = torch.abs(mc_ans - (torch.sigmoid(pred_mc_ans) >= 0.5).float()) #Errors
-        mc_opt_err = 100 * torch.true_divide(diff_mc_ans.sum(), (4*mc_q.size()[0]))
-        mc_q_err = 100 * torch.true_divide((diff_mc_ans.sum(dim=1, keepdim=True) != 0).float().sum(), mc_q.size()[0])
-
-        # Copy the errors from GPU to CPU (sync point).
-        loss, top1_err, top5_err, mc_opt_err, mc_q_err  = (
-            loss.item(),
-            top1_err.item(),
-            top5_err.item(),
-            mc_opt_err.item(),
-            mc_q_err.item()
-        )
+        if has_mc:
+            diff_mc_ans = torch.abs(mc_ans - (torch.sigmoid(pred_mc_ans) >= 0.5).float()) #Errors
+            mc_opt_err = 100 * torch.true_divide(diff_mc_ans.sum(), (4*mc_q.size()[0]))
+            mc_q_err = 100 * torch.true_divide((diff_mc_ans.sum(dim=1, keepdim=True) != 0).float().sum(), mc_q.size()[0])
+            # Copy the stats from GPU to CPU (sync point).
+            loss_des_val, loss_mc_val, top1_err, top5_err, mc_opt_err, mc_q_err  = (
+                loss_des_val.item(),
+                loss_mc_val.item(),
+                top1_err.item(),
+                top5_err.item(),
+                mc_opt_err.item(),
+                mc_q_err.item()
+            )
+            mb_size_mc = mc_q.size()[0]
+        else:
+            mc_opt_err, mc_q_err = None, None
+            mb_size_mc = None
+            loss_des_val, top1_err, top5_err = (
+                loss_des_val.item(),
+                top1_err.item(),
+                top5_err.item()
+            )
 
         val_meter.iter_toc()
-        
+        #top1_err, top5_err, mc_opt_err, mc_q_err, loss_des, loss_mc, mb_size_des, mb_size_mc
         # Update and log stats.
         val_meter.update_stats(
             top1_err,
             top5_err,
             mc_opt_err,
             mc_q_err,
-            des_q.size()[0]
-            * max(
-                cfg.NUM_GPUS, 1
-            ),  # If running  on CPU (cfg.NUM_GPUS == 1), use 1 to represent 1 CPU.
-            loss,
+            loss_des_val,
+            loss_mc_val,
+            des_q.size()[0],
+            mb_size_mc
         )
         val_meter.log_iter_stats(cur_epoch, cur_iter)
         val_meter.iter_tic()
@@ -261,14 +311,14 @@ def eval_epoch(val_loader, model, val_meter, cur_epoch, cfg, test_imp=False):
             print("Top1_err and Top5err")
             print(top1_err, top5_err)
             print("Loss_des_val = {}".format(loss_des_val))
-
-            print(" --- Multiple Choice questions results --- ")
-            print("Mc_q")
-            print(mc_q)
-            print("Mc errors pred x ans")
-            print(torch.abs(mc_ans - (torch.sigmoid(pred_mc_ans) >= 0.5).float()))
-            print("mc_opt_err = {} \nmc_q_err = {}".format(mc_opt_err, mc_q_err))
-            print("Loss_mc_val = {}".format(loss_mc_val))
+            if has_mc:
+                print(" --- Multiple Choice questions results --- ")
+                print("Mc_q")
+                print(mc_q)
+                print("Mc errors pred x ans")
+                print(torch.abs(mc_ans - (torch.sigmoid(pred_mc_ans) >= 0.5).float()))
+                print("mc_opt_err = {} \nmc_q_err = {}".format(mc_opt_err, mc_q_err))
+                print("Loss_mc_val = {}".format(loss_mc_val))
             test_counter += 1
             if test_counter == 4: 
                 break
@@ -316,6 +366,14 @@ def build_clevrer_model(cfg, gpu_id=None):
             module=model, device_ids=[cur_device], output_device=cur_device
         )
     return model
+
+def build_dataloader(cfg, mode):
+    des_dataset = Clevrertext_des(cfg, mode)
+    mc_dataset = Clevrertext_mc(cfg, mode)
+    dataset = Clevrertext_join(des_dataset, mc_dataset)
+    dataloader = DataLoader(dataset, batch_size=cfg.TRAIN.BATCH_SIZE,
+                        shuffle= mode=='train', num_workers=cfg.DATA_LOADER.NUM_WORKERS)
+    return dataloader
 
 
 def train(cfg):
@@ -405,8 +463,11 @@ def test_implementation(cfg):
     start_epoch = cu.load_train_checkpoint(cfg, model, optimizer)
 
     # Create the video train and val loaders.
-    train_loader = loader.construct_loader(cfg, "train")
-    val_loader = loader.construct_loader(cfg, "val")
+    if cfg.TRAIN.DATASET != 'Clevrertext_join':
+        print("This train script does not support your dataset: -{}-. Only Clevrertext_join".format(cfg.TRAIN.DATASET))
+    
+    train_loader = build_dataloader(cfg, "train")
+    val_loader = build_dataloader(cfg, "val")
 
     # Create meters.
     train_meter = ClevrerTrainMeter(len(train_loader), cfg)
