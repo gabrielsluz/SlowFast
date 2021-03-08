27d944e8d1aad81e025ff5b3ad18e007bc911fc6
gabrielsluz <gabrielsluz1999@gmail.com>
2021-03-08 16:24:28 -0300

    Full train

--- clevrer_dev/baselines/hyper_script.py
+++ clevrer_dev/baselines/hyper_script.py
@@ -69,8 +69,8 @@ def get_init_params_cfg():
     cfg.DATA.INPUT_CHANNEL_NUM = [3]
     cfg.DATA.PATH_TO_DATA_DIR = "/datasets/clevrer"
     cfg.DATA.PATH_PREFIX = "/datasets/clevrer"
-    cfg.DATA.MAX_TRAIN_LEN = 40000
-    cfg.DATA.MAX_VAL_LEN = 5000
+    cfg.DATA.MAX_TRAIN_LEN = None
+    cfg.DATA.MAX_VAL_LEN = None
 
     cfg.SOLVER.BASE_LR = 0.001
     cfg.SOLVER.LR_POLICY = "cosine"
