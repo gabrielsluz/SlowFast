817a50420391e1d9ecbc84512a2c43015be72a2a
gabrielsluz <gabrielsluz1999@gmail.com>
2021-03-18 01:51:01 -0300

    Checkpoint and overwrite forward

--- clevrer_dev/feature_extraction/generate_features.py
+++ clevrer_dev/feature_extraction/generate_features.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import torch
 from torch.utils.data import DataLoader
 from slowfast.models.video_model_builder import SlowFast
+import slowfast.utils.checkpoint as cu
 
 from slowfast.utils.parser import load_config, parse_args
 import slowfast.utils.logging as logging
@@ -15,8 +16,25 @@ Example:
 python3 clevrer_dev/feature_extraction/generate_features.py \
   --cfg clevrer_dev/feature_extraction/slowfast.yaml \
   DATA.PATH_TO_DATA_DIR /datasets/clevrer \
-  DATA.PATH_PREFIX /datasets/clevrer
+  DATA.PATH_PREFIX /datasets/clevrer \
+  TRAIN.CHECKPOINT_FILE_PATH model_zoo_ck/SLOWFAST_4x16_R50.pkl \
+  TRAIN.CHECKPOINT_TYPE caffe2
 """
+
+def forward(self, x, bboxes=None):
+    x = self.s1(x)
+    x = self.s1_fuse(x)
+    x = self.s2(x)
+    x = self.s2_fuse(x)
+    for pathway in range(self.num_pathways):
+        pool = getattr(self, "pathway{}_pool".format(pathway))
+        x[pathway] = pool(x[pathway])
+    x = self.s3(x)
+    x = self.s3_fuse(x)
+    x = self.s4(x)
+    x = self.s4_fuse(x)
+    x = self.s5(x)
+    return x
 
 args = parse_args()
 cfg = load_config(args)
@@ -27,10 +45,15 @@ logging.setup_logging(cfg.OUTPUT_DIR)
 dataset = Clevrer_video(cfg, 'train')
 print("Dataset len = {}".format(len(dataset)))
 
+#model = SlowFast(cfg).cuda()
 model = SlowFast(cfg)
+cu.load_test_checkpoint(cfg, model)
+model.eval()
+model.forward = forward.__get__(model, SlowFast)
 
 dataloader = DataLoader(dataset, batch_size=1,
                         shuffle=False, num_workers=0)
 for i_batch, sample_batched in enumerate(dataloader):
-    print(model(sample_batched[0]))
+    print(model(sample_batched[0])[0].size())
+    print(model(sample_batched[0])[1].size())
     break
