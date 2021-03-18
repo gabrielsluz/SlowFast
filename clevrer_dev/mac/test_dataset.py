bccc1a4ad0f1eb45f7227358fde77602b402f123
gabrielsluz <gabrielsluz1999@gmail.com>
2021-03-18 16:10:10 -0300

    Test val dataset

--- clevrer_dev/mac/test_dataset.py
+++ clevrer_dev/mac/test_dataset.py
@@ -11,7 +11,7 @@ import slowfast.utils.logging as logging
 Prints information about the dataset for testing and debugging
 
 Example:
-python3 clevrer_dev/mac/test_clevrer_dataset.py \
+python3 clevrer_dev/mac/test_dataset.py \
   --cfg clevrer_dev/mac/mac.yaml \
   DATA.PATH_TO_DATA_DIR /datasets/clevrer \
   DATA.PATH_PREFIX /datasets/clevrer
@@ -27,8 +27,29 @@ dataset = Clevrermac_des(cfg, 'train')
 print("Dataset len = {}".format(len(dataset)))
 
 #Test DataLoader
-dataloader = DataLoader(dataset, batch_size=1,
-                        shuffle=False, num_workers=0)
+dataloader = DataLoader(dataset, batch_size=2,
+                        shuffle=True, num_workers=0)
+print("Vocab = {}".format(dataset.vocab))
+print("Ans_vocab = {}".format(dataset.ans_vocab))
+
+for i_batch, sampled_batch in enumerate(dataloader):
+    index = sampled_batch['index'].item()
+    print("Video info = {}".format(dataset.get_video_info(index)))
+    print(sampled_batch['slow_ft'].size())
+    print(sampled_batch['fast_ft'].size())
+    print(sampled_batch['question_dict']['question'])
+    print(sampled_batch['question_dict']['ans'])
+    print(sampled_batch['question_dict']['len'])
+    print(sampled_batch['index'])
+
+    break
+
+dataset = Clevrermac_des(cfg, 'val')
+print("Dataset len = {}".format(len(dataset)))
+
+#Test DataLoader
+dataloader = DataLoader(dataset, batch_size=2,
+                        shuffle=True, num_workers=0)
 print("Vocab = {}".format(dataset.vocab))
 print("Ans_vocab = {}".format(dataset.ans_vocab))
 
