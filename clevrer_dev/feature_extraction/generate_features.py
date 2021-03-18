4be22979127c6bcc445e2c4d592851fa45a30c94
gabrielsluz <gabrielsluz1999@gmail.com>
2021-03-18 13:05:07 -0300

    Slowfast pretrained features generation

--- clevrer_dev/feature_extraction/generate_features.py
+++ clevrer_dev/feature_extraction/generate_features.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
-from slowfast.datasets.clevrer_video import Clevrer_video
-import matplotlib.pyplot as plt
 import torch
 from torch.utils.data import DataLoader
+
+import sys
+import os
+
+from tqdm import tqdm
+import h5py
+
+from slowfast.datasets.clevrer_video import Clevrer_video
 from slowfast.models.video_model_builder import SlowFast
 import slowfast.utils.checkpoint as cu
-
 from slowfast.utils.parser import load_config, parse_args
 import slowfast.utils.logging as logging
 
+
 """
-Prints information about the dataset for testing and debugging
+Generates SlowFast features
 
 Example:
 python3 clevrer_dev/feature_extraction/generate_features.py \
@@ -21,6 +27,7 @@ python3 clevrer_dev/feature_extraction/generate_features.py \
   TRAIN.CHECKPOINT_TYPE caffe2
 """
 
+#SlowFast feature extraction from almost last layer
 def forward(self, x, bboxes=None):
     x = self.s1(x)
     x = self.s1_fuse(x)
@@ -36,26 +43,63 @@ def forward(self, x, bboxes=None):
     x = self.s5(x)
     return x
 
-args = parse_args()
-cfg = load_config(args)
 
-logger = logging.get_logger(__name__)
-logging.setup_logging(cfg.OUTPUT_DIR)
+def gen_dataset(cfg, mode, root):
+    #Generates two datasets for a certain split. => Slow and fast features
+    #When using the generated file must indicate in which index the dataset starts to work
+    #Train starts in 0
+    #Val starts in 10000
+    #Test starts in 15000
+    dataset = Clevrer_video(cfg, mode)
+    print("Dataset {} len = {}".format(mode, len(dataset)))
 
-dataset = Clevrer_video(cfg, 'train')
-print("Dataset len = {}".format(len(dataset)))
+    dataloader = DataLoader(dataset, batch_size=cfg.TRAIN.BATCH_SIZE,
+                            shuffle=False, num_workers=cfg.DATA_LOADER.NUM_WORKERS)
+    size = len(dataloader)
+    batch_size = cfg.TRAIN.BATCH_SIZE
 
-#model = SlowFast(cfg).cuda()
-model = SlowFast(cfg)
-cu.load_test_checkpoint(cfg, model)
-model.eval()
-model.forward = forward.__get__(model, SlowFast)
+    #h5py slow and fast datasets
+    #Slow
+    slow_path = os.path.join(root, '{}_slow_features.hdf5'.format(mode))
+    slow_h5 = h5py.File(slow_path, 'w', libver='latest')
+    slow_dset = slow_h5.create_dataset('data', (size * batch_size, 2048, 4, 7, 7),
+                            dtype='f4')
+    #Fast
+    fast_path = os.path.join(root, '{}_fast_features.hdf5'.format(mode))
+    fast_h5 = h5py.File(fast_path, 'w', libver='latest')
+    fast_dset = fast_h5.create_dataset('data', (size * batch_size, 256, 32, 7, 7),
+                            dtype='f4')
 
-dataloader = DataLoader(dataset, batch_size=1,
-                        shuffle=False, num_workers=0)
-for i_batch, sampled_batch in enumerate(dataloader):
-    print(sampled_batch[0][0].size())
-    out = model(sampled_batch[0])
-    print(out[0].size())
-    print(out[1].size())
-    break
+    with torch.no_grad():
+        for i, sampled_batch in tqdm(enumerate(dataloader)):
+            out = model(sampled_batch[0])
+            slow_ft = out[0].detach().cpu().numpy()
+            fast_ft = out[1].detach().cpu().numpy() 
+            slow_dset[i * batch_size:(i + 1) * batch_size] = slow_ft
+            fast_dset[i * batch_size:(i + 1) * batch_size] = fast_ft
+
+            break
+    slow_h5.close()
+    fast_h5.close()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    cfg = load_config(args)
+
+    logger = logging.get_logger(__name__)
+    logging.setup_logging(cfg.OUTPUT_DIR)
+    use_gpu = cfg.NUM_GPUS > 0
+    #Set model 
+    model = SlowFast(cfg)
+    if use_gpu:
+        cur_device = torch.cuda.current_device()
+        model = model.cuda(device=cur_device)
+    cu.load_test_checkpoint(cfg, model)
+    model.forward = forward.__get__(model, SlowFast)
+    model.eval()
+
+    #Proccess datasets
+    root = cfg.DATA.PATH_TO_DATA_DIR
+    gen_dataset(cfg, 'train', root)
+    gen_dataset(cfg, 'val', root)
