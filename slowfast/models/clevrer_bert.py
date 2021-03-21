a9c9d8d9a70cb781d255f6b56e6ab94017264d38
gabrielsluz <gabrielsluz1999@gmail.com>
2021-03-21 17:12:45 -0300

    Wrong init

--- slowfast/models/clevrer_bert.py
+++ slowfast/models/clevrer_bert.py
@@ -244,10 +244,10 @@ class BERT_CNN_MAC(nn.Module):
         self.reset()
 
     def reset(self):
-        kaiming_uniform_(self.conv[0].weight)
-        self.conv[0].bias.data.zero_()
-        kaiming_uniform_(self.conv[2].weight)
-        self.conv[2].bias.data.zero_()
+        # kaiming_uniform_(self.conv[0].weight)
+        # self.conv[0].bias.data.zero_()
+        # kaiming_uniform_(self.conv[2].weight)
+        # self.conv[2].bias.data.zero_()
 
         kaiming_uniform_(self.classifier[0].weight)
 
